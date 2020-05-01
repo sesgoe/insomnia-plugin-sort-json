de5d32831cca03943f9a43d1fdac4298ef403f65
sesgoe <jsesgoe@gmail.com>
2020-05-01 10:02:15 -0400

    Pass response body even if it fails to parse JSON

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ module.exports.responseHooks = [
     context => {
 
         const body = context.response.getBody()
-        const data = JSON.parse(Buffer.from(body).toString())
-        const sortedData = Buffer.from(JSON.stringify(jsonabc.sortObj(data)))
+        let data = {}
 
+        try {
+            data = JSON.parse(Buffer.from(body).toString())
+        } catch (e) {
+            context.response.setBody(body) //if the JSON fails to parse, pass response body through anyway
+            return
+        }
+
+        const sortedData = Buffer.from(JSON.stringify(jsonabc.sortObj(data)))
         context.response.setBody(sortedData)
 
     }
