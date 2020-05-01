e179c59c62335ea373bd0b1e2822fdb5201ed434
sesgoe <jsesgoe@gmail.com>
2020-05-01 14:18:07 -0400

    Updated to sort-json, updated README

--- README.md
+++ README.md
@@ -1,14 +1,36 @@
 # Insomnia - Sort JSON Responses
-This plugin sorts the properties of JSON responses in alphabetical ascending order using `jsonabc`.
+This plugin sorts the properties of JSON responses using `sort-json`.
 
-If `JSON.parse()` fails, the plugin fails fast and passes the response body through as-is.
+`sort-json` can be found at https://www.npmjs.com/package/sort-json
 
-`jsonabc` can be found here: https://github.com/ShivrajRath/jsonabc
-
-## Install
+## ✅ Install
 1. Go to `Insomnia/Preferences` and choose the *Plugins* tab
 2. Enter `insomnia-plugin-sort-json` and click on `Install Plugin`
 3. That's it, enjoy!
 
-## Update Plugin Version
+## ⏬ Update Plugin Version
 1. Follow the same instructions for Installing!
+
+
+##  🛠 Configuration
+1. Use the large "Insomnia" button in the top left
+2. Under Plugins near the bottom, you'll see a few options:
+
+- 🔼 **sort-json ascending**
+  - Sorts the resulting JSON response in alphabetical ascending order
+- 🔽 **sort-json descending**
+  - Sorts the resulting JSON response in alphabetical descending order
+- 🅰️ **sort-json ignore case**
+  - Ignores uppercases in JSON property names when sorting
+- 🅰️ **sort-json no-ignore-case**
+  - Does *not* ignore uppercases in JSON property names when sorting
+- ⚙️ **sort-json reset to defaults**
+  - Resets all settings for sort-json to their defaults
+    - `sort-ascending`: true
+    - `sort-depth`: 5
+    - `ignore-case`: true
+
+### ⚠️ Special Configuration Note
+`sort-depth` is currently not configurable through a nice UI in Insomnia. You can open the plugins folder directly and change the value of `sortDepthDefault` inside `index.js` if you'd like to change the value. The `setUpDefaults` will just always set the value of `sort-depth` to whatever you configure the default to be. This is a stop-gap until Insomia updates their `context.app` api to include prompt windows from `workspaceAction` selections.
+
+`sort-depth` default value is 5, which means it will sort up to 5 levels deep inside a returned JSON object.
--- index.js
+++ index.js
@@ -1,20 +1,117 @@
-const jsonabc = require('jsonabc')
+const sortJson = require('sort-json')
+
+const sortAscendingKey  = 'sort-ascending'
+const sortDepthKey          = 'sort-depth'
+const ignoreCaseKey     = 'ignore-case'
+
+const sortAscendingDefault  = true
+const sortDepthDefault      = 5
+const ignoreCaseDefault     = true
+
+const setConfigValue = async (context, key, value) => {
+    await context.store.setItem(key, value)
+    console.warn(`Set sort-json ${key} to ${value}`)
+}
+
+const setUpDefaults = async (context) => {
+    if(!await context.store.hasItem(sortAscendingKey)) {
+        await setConfigValue(context, sortAscendingKey, sortAscendingDefault)
+    }
+
+    //for now, always set the sortDepth to default in case people want to change it easily
+    await setConfigValue(context, sortDepthKey, sortDepthDefault)
+
+    if(!await context.store.hasItem(ignoreCaseKey)) {
+        setConfigValue(context, ignoreCaseKey, ignoreCaseDefault)
+    }
+}
+
+const getConfigBoolean = async (context, key) => {
+    return await context.store.getItem(key) == 'true'
+}
+
+const getConfigInt = async (context, key) => {
+    return parseInt(await context.store.getItem(key))
+}
 
 module.exports.responseHooks = [
-    context => {
+    async (context) => {
 
-        const body = context.response.getBody()
+        //stopgap until insomnia can give us a load hook with context
+        await setUpDefaults(context)
+
+        const body = await context.response.getBody()
         let data = {}
 
         try {
             data = JSON.parse(Buffer.from(body).toString())
         } catch (e) {
-            context.response.setBody(body) //if the JSON fails to parse, pass response body through anyway
-            return
+            return await context.response.setBody(body) //if the JSON fails to parse, pass response body through anyway
         }
 
-        const sortedData = Buffer.from(JSON.stringify(jsonabc.sortObj(data)))
-        context.response.setBody(sortedData)
+        //sort-ascending means reverse = false
+        const options = {
+            reverse: !await getConfigBoolean(context, sortAscendingKey),
+            depth: await getConfigInt(context, sortDepthKey),
+            ignoreCase: await getConfigBoolean(context, ignoreCaseKey)
+        }
+
+        const sortedData = Buffer.from(JSON.stringify(sortJson(data, options)))
+        await context.response.setBody(sortedData)
 
     }
+]
+
+
+module.exports.workspaceActions = [
+    {
+        label: "sort-json ascending (default)",
+        icon: "fa-chevron-up",
+        action: async (context, _) => {
+            await setConfigValue(context, sortAscendingKey, true)
+        }
+    },
+    {
+        label: "sort-json descending",
+        icon:"fa-chevron-down",
+        action: async(context, _) => {
+            await setConfigValue(context, sortAscendingKey, false)
+        }
+    },
+    // including below for when context.app.prompt is fixed for workspaceActions
+    // {
+    //     label: "sort-json depth",
+    //     icon: "fa-chevron-right",
+    //     action: async (context, data) => {
+    //         const result = await context.app.prompt('Configure Sort Depth', {
+    //           label: 'sort-json sort depth',
+    //           defaultValue: '1',
+    //           submitName: 'Set',
+    //           cancelable: true
+    //         })
+    //       }
+    // },
+    {
+        label: "sort-json ignore-case (default)",
+        icon:"fa-font",
+        action: async(context, _) => {
+            await setConfigValue(context, ignoreCaseKey, true)
+        }
+    },
+    {
+        label: "sort-json NO-ignore-case",
+        icon:"fa-font",
+        action: async(context, _) => {
+            await setConfigValue(context, ignoreCaseKey, false)
+        }
+    },
+    {
+        label: "sort-json reset to defaults",
+        icon:"fa-cogs",
+        action: async(context, _) => {
+            await setConfigValue(context, sortAscendingKey, sortAscendingDefault)
+            await setConfigValue(context, sortDepthKey, sortDepthDefault)
+            await setConfigValue(context, ignoreCaseKey, ignoreCaseDefault)
+        }
+    },
 ]
